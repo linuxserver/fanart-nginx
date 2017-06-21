f98db4be29601f826df4b03606b8eac5b9c107aa
aptalca <aptalca@users.noreply.github.com>
2017-06-21 14:10:58 -0400

    Update distribute.sh

--- root/defaults/distribute.sh
+++ root/defaults/distribute.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/with-contenv bash
 
 # placeholder script to distribute the cert to slave IPs
+
+. /config/donoteditthisfile.conf
+
+for job in $(echo "$SLAVEIPS" | tr "," " "); do
+  . /config/distribute/"$job"/slave.conf
+  scp -i /config/distribute/"$job"/private -P "$SSHPORT" -r /config/etc/letsencrypt/live /config/etc/letsencrypt/archive "$SSHUSER"@"$job":"$CONFIGPATH"/etc/letsencrypt
+  ssh -i /config/distribute/"$job"/private -p "$SSHPORT" "$SSHUSER"@"$job" 'docker exec -it fanartstatic s6-svc -h /var/run/s6/services/nginx'
+done
