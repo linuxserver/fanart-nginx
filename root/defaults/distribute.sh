6b61219491e23544b1065c8c9714ead4dca88c4e
aptalca <aptalca@users.noreply.github.com>
2017-06-27 01:22:17 -0400

    remove pseudo tty

--- root/defaults/distribute.sh
+++ root/defaults/distribute.sh
@@ -7,5 +7,5 @@
 for job in $(echo "$SLAVEIPS" | tr "," " "); do
   . /config/distribute/"$job"/slave.conf
   scp -i /config/distribute/"$job"/private -P "$SSHPORT" -oStrictHostKeyChecking=no -r /config/etc/letsencrypt/live /config/etc/letsencrypt/archive "$SSHUSER"@"$job":"$CONFIGPATH"/etc/letsencrypt
-  ssh -i /config/distribute/"$job"/private -p "$SSHPORT" -oStrictHostKeyChecking=no "$SSHUSER"@"$job" 'docker exec -it fanartstatic s6-svc -h /var/run/s6/services/nginx'
+  ssh -i /config/distribute/"$job"/private -p "$SSHPORT" -oStrictHostKeyChecking=no "$SSHUSER"@"$job" 'docker exec -i fanartstatic s6-svc -h /var/run/s6/services/nginx'
 done
