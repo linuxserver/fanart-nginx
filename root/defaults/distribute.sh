2b3a54c98de4aff5e84314e2b2fb13111686fb2a
aptalca <aptalca@users.noreply.github.com>
2017-06-27 13:27:51 -0400

    distribute all letsencrypt files, not just the certs

--- root/defaults/distribute.sh
+++ root/defaults/distribute.sh
@@ -6,6 +6,7 @@
 
 for job in $(echo "$SLAVEIPS" | tr "," " "); do
   . /config/distribute/"$job"/slave.conf
-  scp -i /config/distribute/"$job"/private -P "$SSHPORT" -oStrictHostKeyChecking=no -r /config/etc/letsencrypt/live /config/etc/letsencrypt/archive "$SSHUSER"@"$job":"$CONFIGPATH"/etc/letsencrypt
-  ssh -i /config/distribute/"$job"/private -p "$SSHPORT" -oStrictHostKeyChecking=no "$SSHUSER"@"$job" 'docker exec -i fanartstatic s6-svc -h /var/run/s6/services/nginx'
+  ssh -i /config/distribute/"$job"/private -p "$SSHPORT" -oStrictHostKeyChecking=no "$SSHUSER"@"$job" 'rm -rf "$CONFIGPATH"/etc/letsencrypt'
+  scp -i /config/distribute/"$job"/private -P "$SSHPORT" -oStrictHostKeyChecking=no -r /config/etc/letsencrypt "$SSHUSER"@"$job":"$CONFIGPATH"/etc/letsencrypt
+  ssh -i /config/distribute/"$job"/private -p "$SSHPORT" -oStrictHostKeyChecking=no "$SSHUSER"@"$job" 'docker exec -i chown -R abc:abc /config/etc/letsencrypt && fanartstatic s6-svc -h /var/run/s6/services/nginx'
 done
